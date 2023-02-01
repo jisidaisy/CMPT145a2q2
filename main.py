0f99882ad47ffca8c04d36201298a852e6c08533
jisidaisy <jisidaisy@yahoo.com>
2023-02-01 17:46:33 -0600

    Open file to be read
    Added the line of code to open the file to be read

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import sys
 if len(sys.argv) != 2:
+    # Checking if filename is given as argument
     print("Must include filename as an argument")
     sys.exit(1)
+
+# Opening example file
+with open('example.txt', 'r') as f:
+    # Going through the file line by line
